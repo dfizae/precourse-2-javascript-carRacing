a9643ce9c9435e4bba166491af3b8434ebd9c17a
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-10-22 01:00:35 +0900

    fix: 실행 결과 출력 부분 수정

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ class App {
   async run() {
     const PARTICIPATIONS = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분');
     const TRIAL = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?'));
-    const RESULT = this.race(PARTICIPATIONS, TRIAL);
-    await MissionUtils.Console.print(`실행 결과\n${RESULT}`);
+    await MissionUtils.Console.print(`실행 결과\n`);
+    this.race(PARTICIPATIONS, TRIAL);
   }
 
   race(participations, trial){
     
-    let participtionList = participations.split(',');
+    let participationList = participations.split(',');
     let i = 1;
     
-    for(let x of participtionList){
-
+    for(let x of participationList){
+      let name = x;
     }
     
     while(i <= trial){
