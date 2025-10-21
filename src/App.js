027eec0ceff177b3f61a7e3002121300d2583d76
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-10-22 00:54:05 +0900

    fix: 시도 횟수 입력과 실행 결과 출력 관련 수정

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const PARTICIPATIONS = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분');
-    const TRIAL = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    const TRIAL = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?'));
     const RESULT = this.race(PARTICIPATIONS, TRIAL);
-    await MissionUtils.Console.print(`실행 결과/n${RESULT}`);
+    await MissionUtils.Console.print(`실행 결과\n${RESULT}`);
   }
 
   race(participations, trial){
