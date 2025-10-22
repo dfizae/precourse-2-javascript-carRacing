90def2e5996693c3ffaf530c067924e03d427a7f
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-10-22 18:23:10 +0900

    fix: run 메서드 입력 형식 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const PARTICIPATIONS = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분');
-    const TRIAL = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?'));
+    const PARTICIPATIONS = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n');
+    const TRIAL = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
     await MissionUtils.Console.print(`실행 결과\n`);
     this.race(PARTICIPATIONS, TRIAL);    
   }
@@ -20,7 +20,7 @@ class App {
   }
 
   forward(racer){
-    racer.forEach( racer => {
+    racer.forEach(racer => {
       const RANDOMVALUE = MissionUtils.Random.pickNumberInRange(0, 9);
       if(RANDOMVALUE >= 4) racer.position++;
     });
