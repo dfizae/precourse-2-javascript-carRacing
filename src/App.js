28e9cfdd265ea9172f384aa77f6464363e8cb6b6
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-10-22 18:52:59 +0900

    fix: const 변수명 표기법 수정

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const PARTICIPATIONS = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n');
-    const TRIAL = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
+    const participations = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n');
+    const trial = Number(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
     await MissionUtils.Console.print(`실행 결과\n`);
-    this.race(PARTICIPATIONS, TRIAL);    
+    this.race(participations, trial);    
   }
 
   race(participations, trial){
     
-    const RACER = participations.split(',').map(name => {
+    const racer = participations.split(',').map(name => {
       if(name.length > 5) throw new Error("[ERROR] : 레이서 명이 5자 이하만 가능합니다..");
       return { name, position: 0 };
     });
     
     for(let i = 0; i < trial; i++){
-      this.forward(RACER);
-      this.printRound(RACER);
+      this.forward(racer);
+      this.printRound(racer);
     }
 
   }
